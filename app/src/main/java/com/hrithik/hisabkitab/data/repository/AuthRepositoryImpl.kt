596a13d1982942f75d0c97cb85a8f4555588361d
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-07-18 13:52:49 +0530

    Create repository for firebase authentication

---
+++ app/src/main/java/com/hrithik/hisabkitab/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.hrithik.hisabkitab.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.hrithik.hisabkitab.util.Resource
+import jakarta.inject.Inject
+import kotlinx.coroutines.tasks.await
+
+
+class AuthRepositoryImpl @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : AuthRepository {
+
+    override val currentUser: FirebaseUser?
+        get() = firebaseAuth.currentUser
+
+    override suspend fun signup(name: String, email: String, password: String): Resource<FirebaseUser> {
+        return try {
+            val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+            result.user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(name).build())?.await()
+            return Resource.Success(result.user!!)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Failure(e)
+        }
+    }
+
+    override suspend fun login(email: String, password: String): Resource<FirebaseUser> {
+        return try {
+            val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+            Resource.Success(result.user!!)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Failure(e)
+        }
+    }
+
+    override fun logout() {
+        firebaseAuth.signOut()
+    }
+}
--- app/src/main/java/com/hrithik/hisabkitab/di/AppModule.kt
+++ app/src/main/java/com/hrithik/hisabkitab/di/AppModule.kt
@@ -1,6 +1,8 @@
 package com.hrithik.hisabkitab.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.hrithik.hisabkitab.data.repository.AuthRepository
+import com.hrithik.hisabkitab.data.repository.AuthRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,4 +16,7 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    fun providesAuthRepository(impl: AuthRepositoryImpl): AuthRepository = impl
 }
