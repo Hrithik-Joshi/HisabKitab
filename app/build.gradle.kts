ce02460a22ebc2d578eadc1a47b29a8297862617
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-07-18 13:19:26 +0530

    setup hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.android)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
