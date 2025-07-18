2e31dd2c5ca371e831ac375371df0ba3cd5ca5c1
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-07-18 12:44:44 +0530

    setup hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
     id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -59,6 +61,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.hilt.android)
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 
     //firebase
     implementation(platform(libs.firebase.bom))
