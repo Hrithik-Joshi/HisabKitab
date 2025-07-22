1b58b577a4f0518a9692e91671f5b5df6570cc54
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-07-23 00:28:59 +0530

    add gradient to the cards

--- app/src/main/java/com/hrithik/hisabkitab/ui/compose/GradientColor.kt
+++ app/src/main/java/com/hrithik/hisabkitab/ui/compose/GradientColor.kt
@@ -8,19 +8,19 @@ class GradientColor() {
     companion object {
 
         val expenseGradient = Brush.horizontalGradient(
-            listOf(Color(0xFFea384d), Color(0xFFd31027))
+            listOf(Color(0xFFbc556f), Color(0xFFf9a470))
         )
 
         val incomeGradient = Brush.horizontalGradient(
-            listOf(Color(0xFF56ab2f), Color(0xFFa8e063))
+            listOf(Color(0xFF5fc52e), Color(0xFF6eee87))
         )
         val loanGradient = Brush.horizontalGradient(
-            listOf(Color(0xFF2193b0), Color(0xFF6dd5ed))
+            listOf(Color(0xFF5ab2f7), Color(0xFF12cff3))
         )
 
 
         val investmentGradient = Brush.horizontalGradient(
-            listOf(Color(0xFFff9966), Color(0xFFff5e62))
+            listOf(Color(0xFFf4762d), Color(0xFFffd78a))
         )
     }
 }
